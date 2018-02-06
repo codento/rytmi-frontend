6f6ae03d12bed5400f2d0e4c811e4f940d6a5306
Eero Pukkila <eero.pukkila@codento.com>
2018-02-07 00:39:17 +0200

    search lists profiles and can be filtered

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,5 +1,6 @@
 export const FETCH_PROFILES = 'profiles/FETCH_PROFILES'
 export const UPDATE_PROFILE = 'profiles/UPDATE_PROFILE'
 export const FETCH_PROFILESKILLS = 'profiles/FETCH_PROFILESKILLS'
+export const UPDATE_PROFILELIST = 'profiles/UPDATE_PROFILELIST'
 export const FETCH_SKILLS = 'skills/FETCH_SKILLS'
 export const SET_LOADING = 'SET_LOADING'
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,71 @@
 <template>
   <div class="animated fadeIn">
-    Hello Search!
+    <b-col class="col-sm-3" style="float:left">
+      <b-card>
+        <h2>Search</h2>
+        <p>(Filters by name)</p>
+        <input v-model="param"></input>
+      </b-card>
+    </b-col>
+    <b-col class="col-sm-9" style="float:right">
+      <ul>
+        <li v-for='profile in profileFilter(param)'>
+          <b-card>
+            <b-row>
+              <b-col class="col-sm-4" style="text-align:center">
+                <img :src='profile.photoPath' alt="">
+                <h3>{{profile.firstName}} {{profile.lastName}}</h3>
+                <b>Email: {{profile.email}}</b><br>
+                <b>puhelinnumero: {{profile.phone}}</b>
+              </b-col>
+              <b-col class="skills">
+                <SkillRow class="col-sm-6" v-for='skill in skills' :name="skill.name" :proficiency='Math.floor(6 * Math.random())' :key='skill.id'></SkillRow>
+              </b-col>
+            </b-row>
+          </b-card>
+        </li>
+      </ul>
+    </b-col>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Search'
+import { mapGetters } from 'vuex'
+import SkillRow from '../components/SkillRow'
+
+export default {
+  name: 'Search',
+  data () {
+    return {
+      param: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'profiles',
+      'profileById',
+      'profileFilter',
+      'skills'
+    ])
+  },
+  components: {
+    SkillRow
+  },
+  created () {
+    this.$store.dispatch('fetchProfiles')
+    this.$store.dispatch('fetchSkills')
+  },
+  methods: {
   }
+}
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+img {
+  object-fit: contain;
+  margin-left: 10px
+}
+</style>
