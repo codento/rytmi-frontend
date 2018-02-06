ae6800ce7e8562286823b435e11b1b9425744de2
Eero Pukkila <eero.pukkila@codento.com>
2018-02-07 00:39:17 +0200

    search lists profiles and can be filtered

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,7 @@
 export const FETCH_PROFILES = 'profiles/FETCH_PROFILES'
 export const UPDATE_PROFILE = 'profiles/UPDATE_PROFILE'
 export const FETCH_PROFILESKILLS = 'profiles/FETCH_PROFILESKILLS'
+export const UPDATE_PROFILELIST = 'profiles/UPDATE_PROFILELIST'
 export const FETCH_SKILLS = 'skills/FETCH_SKILLS'
 export const SET_LOADING = 'SET_LOADING'
 export const ADD_PROFILE_SKILL = 'profiles/ADD_PROFILESKILLS'
