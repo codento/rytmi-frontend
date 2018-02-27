d53ea795ca06c400c68cc517ce65baedc2bb8b16
Riku Kalander <riku.kalander@codento.com>
2018-02-27 14:04:23 +0200

    moved stuff to header, created simple 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="animated fadeIn container" style="margin-top: 24px">
+    <b-jumbotron  bg-variant="info" text-variant="white" border-variant="dark" header="404 sivua ei löytynyt" >
+     
+</b-jumbotron>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
