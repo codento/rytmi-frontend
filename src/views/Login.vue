697512f47332dc18fd1f7d8d810d0002b962bb5f
Riku Kalander <riku.kalander@codento.com>
2018-02-27 14:04:23 +0200

    moved stuff to header, created simple 404 page

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="animated fadeIn container" style="margin-top: 24px">
+    <b-jumbotron  bg-variant="info" text-variant="white" border-variant="dark" header="Rytmiryhmä app" lead="Please login to continue" >
+        <p>For more information visit website</p>
+</b-jumbotron>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
