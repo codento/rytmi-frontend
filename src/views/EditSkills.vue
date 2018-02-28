9430884635da54198e3cc6a989fb9786201e65c4
Mikko Kautto <mikko.kautto@codento.com>
2018-02-28 17:13:22 +0200

    Fur hat version of adding skills to profile

--- src/views/EditSkills.vue
+++ src/views/EditSkills.vue
@@ -1,23 +1,88 @@
-
-'
 <template>
   <div class="animated fadeIn">
     <b-container fluid>
-      <h1>Taidot</h1>
-      <b-row > 
-        <b-col v-for="skill, index in skillsByUserId(profileId)"  class="col-sm-3">
+      <h1>Skills</h1>
+      <b-row>
+        <b-col v-for="skill, index in skillsByUserId(profileId)" class="col-sm-3">
           <div v-on:click="deleteSkill(index)">
-          <skill-card :skill="skill" />
+            <skill-card :skill="skill" class="skillCard" />
           </div>
         </b-col>
         <b-col class="col-sm-3">
           <b-card
-          class="addNew"
-          title="Add new"
-          v-on:click="addNew()"
-          />
-        </b-col>       
-      </b-row>  
+            class="addNew"
+            title="Add new">
+            <b-form @submit="onSubmit">
+              <b-form-group
+                vertical
+                id="skillLabel"
+                label="Skill:"
+                label-for="skill">
+                <b-form-select
+                  id="skill"
+                  v-model="profileSkill.skillId" 
+                  :options="availableSkills"
+                  value-field="id"
+                  text-field="name">
+                  <template slot="first">
+                    <option :value="null" disabled>-- Select skill --</option>
+                  </template>
+                </b-form-select>
+              </b-form-group>
+              <b-form-group 
+                id="description" 
+                vertical
+                label="Description:"
+                label-for="descriptionInput">
+                <b-form-textarea
+                    id="descriptionInput"
+                    :rows="3"
+                    type="text"
+                    v-model="profileSkill.description"
+                    placeholder="Short description">
+                </b-form-textarea>
+              </b-form-group>
+              <b-form-group 
+                id="wantsToLabel" 
+                vertical
+                label="Wants:"
+                label-for="wantsTo">
+                <b-form-input 
+                  id="wantsTo"
+                  type="range"
+                  min="0"
+                  max="5"
+                  v-model="profileSkill.wantsTo">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group 
+                id="knowsLabel" 
+                vertical
+                label="Knows:"
+                label-for="knows">
+                <b-form-input 
+                  id="knows"
+                  type="range"
+                  min="0"
+                  max="5"
+                  v-model="profileSkill.knows">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group 
+                id="visibleInCVLabel" 
+                vertical
+                label="Show in CV:"
+                label-for="visibleInCV">
+                <input type="checkbox" 
+                  id="visibleInCV"
+                  v-model="profileSkill.visibleInCV">
+              </b-form-group>
+              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-button @click="onReset" variant="danger">Clear</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -26,8 +91,18 @@
 import {mapGetters, mapActions} from 'vuex'
 import SkillCard from '../components/SkillCard'
 
+const skillTemplate = () => {
+  return {
+    skillId: null,
+    knows: 0,
+    wantsTo: 0,
+    description: '',
+    visibleInCV: true
+  }
+}
+
 export default {
-  name: 'EditProfile',
+  name: 'EditSkills',
   components: {
     SkillCard
   },
@@ -38,30 +113,32 @@ export default {
     ...mapGetters([
       'skills',
       'skillsByUserId'
-    ])
+    ]),
+    availableSkills () {
+      const existingIds = this.skillsByUserId(this.profileId)
+        .map(profileSkill => profileSkill.skillId)
+      return this.$lodash.filter(this.skills, (value, key) => {
+        return existingIds.indexOf(value.id) === -1
+      })
+    }
   },
   methods: {
     ...mapActions([
       'addProfileSkill',
       'deleteProfileSkill'
     ]),
-    addNew () {
-      const request = {
-        id: this.profileId,
-        body: {
-          skillId: Math.floor(Math.random() * Object.keys(this.skills).length) - 1,
-          knows: Math.floor(Math.random() * 5),
-          wantsTo: Math.floor(Math.random() * 3) + 1,
-          visibleInCV: true,
-          description: 'blah'
-        }
-      }
-      this.addProfileSkill(request)
+    onSubmit () {
+      this.profileSkill.profileId = this.profileId
+      this.addProfileSkill(this.profileSkill)
+      this.profileSkill = skillTemplate()
+    },
+    onReset () {
+      this.profileSkill = skillTemplate()
     }
   },
   data () {
     return {
-      showModal: false
+      profileSkill: skillTemplate()
     }
   }
 }
@@ -69,7 +146,7 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-.card :hover{
+.skillCard :hover{
   background-color: hsla(34, 100%, 51%, 0.541);
 
 }
