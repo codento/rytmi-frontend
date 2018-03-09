82131203a5faadce6d0940c4509f5fb095c3a31d
Tom Thureson <tuhmm@Toms-MacBook-Pro.local>
2018-03-09 14:11:33 +0200

    Moved api-calls to an own module from actions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
+import { denormalize } from 'normalizr'
+import { profile, skill } from '../store/schema'
 
 const PATH_AUTH = '/auth'
+const PATH_SKILLS = '/skills'
+const PATH_PROFILES = '/profiles'
+const PATH_PROFILESKILLS = '/profileskills'
 
 export function login (token) {
   return axios.post(process.env.API_URL + PATH_AUTH, {id_token: token})
@@ -9,4 +14,25 @@ export function login (token) {
     }).catch(response => {
       console.log('Backend error' + response)
     })
+}
+
+export function getSkills (token) {
+  return axios.get(process.env.API_URL + PATH_SKILLS)
+}
+
+export function getProfiles (token) {
+  return axios.get(process.env.API_URL + PATH_PROFILES)
+}
+
+export function getProfileSkills (token) {
+  return axios.get(process.env.API_URL + PATH_PROFILESKILLS)
+}
+
+export function alterProfile (data, token) {
+  console.log(data)
+  return axios.put(process.env.API_URL + PATH_PROFILES + '/' + data.id, denormalize(data, [profile]))
+}
+
+export function newProfileSkill (data, token) {
+  return axios.post(process.env.API_URL + PATH_PROFILES + '/' + data.profileId + PATH_SKILLS, denormalize(data, [skill]))
 }
