0d4ef1fc573c9f032204bec0699b623451b42a1a
Riku Kalander <riku.kalander@codento.com>
2018-03-13 11:27:48 +0200

    items retrieved from store after logging in

---
+++ src/components/callback.vue
@@ -0,0 +1,22 @@
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  mounted () {
+    console.log('clalback')
+    this.$nextTick(() => {
+      this.fetchProfiles()
+      this.fetchSkills()
+      this.fetchProfileSkills()
+      this.$router.push(this.$route.query.redirect || '/')
+    })
+  },
+  methods: {
+    ...mapActions([
+      'fetchProfiles',
+      'fetchSkills',
+      'fetchProfileSkills'
+    ])
+  }
+}
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -36,7 +36,6 @@ export default {
   computed: {
     ...mapGetters([
       'profileById',
-      'profiles',
       'skillById',
       'skillsByUserId'
     ])
