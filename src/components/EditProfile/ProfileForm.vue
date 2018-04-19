1b5518a1f1fd96a5b89cf7d03152ee6ae53cf07d
Ilari Nieminen <ilari.nieminen@codento.com>
2018-04-19 08:37:29 +0300

    Chaged peruuta -> reset on profile form

--- src/components/EditProfile/ProfileForm.vue
+++ src/components/EditProfile/ProfileForm.vue
@@ -103,7 +103,7 @@
       </b-form-input>
     </b-form-group>-->
     <b-button type="submit" variant="primary">Submit</b-button>
-    <b-button type="reset" variant="danger">Peruuta</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
   </b-form>
 </template>
 
