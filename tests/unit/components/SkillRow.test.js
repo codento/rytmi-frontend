562e64e527ca1541addb0f02ea35ecb1121f8e97
Lauri Kajan <lauri.kajan@codento.com>
2018-10-15 10:28:29 +0300

    add bootstrap

--- tests/unit/components/SkillRow.test.js
+++ tests/unit/components/SkillRow.test.js
@@ -1,10 +1,12 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import { SkillRow } from '../../../src/components/Profile'
 import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
 
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
+localVue.use(BootstrapVue)
 
 const getters = {
   profileById: () => (arg) => arg,
