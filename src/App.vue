ecded00b8366d7d586e83cb3ac5ee30bc57a1c13
Valtteri Lilja <valtteri.lilja@gmail.com>
2018-10-31 09:14:14 +0200

    changed edit profile route to make more sense

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default {
           },
           {
             name: 'Edit Profile',
-            url: '/edit/' + this.profileId,
+            url: '/profile/' + this.profileId + '/edit',
             icon: 'icon-pencil'
           }
         ]
