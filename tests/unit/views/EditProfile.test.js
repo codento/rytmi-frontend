4c1545bcf6769ed3bafd6a84f13a5ec139e8b270
Valtteri Lilja <valtteri.lilja@gmail.com>
2018-10-31 14:55:21 +0200

    Added test for EditProfile.vue

--- tests/unit/views/EditProfile.test.js
+++ tests/unit/views/EditProfile.test.js
@@ -14,17 +14,13 @@ const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 localVue.use(Vuex)
 
+const mockProfile = { id: 5 }
+
 function createStore (overrideConfig) {
   const getters = {
-    profileById: () => () => { id: 5 }
+    profileById: () => () => mockProfile
   }
-  const actions = {
-
-  }
-  const mutations = {
-
-  }
-  const mergedStoreConfig = merge({ getters, actions, mutations }, overrideConfig)
+  const mergedStoreConfig = merge({ getters }, overrideConfig)
   return new Vuex.Store(mergedStoreConfig)
 }
 
@@ -38,12 +34,39 @@ function createWrapper (overrideOptions) {
 }
 
 describe('EditProfile.vue', () => {
+  it('should not display any subcomponents when profile cannot be found', () => {
+    const propsData = {
+      profileId: 'unknownprofile'
+    }
+    const store = createStore({
+      getters: {
+        profileById: () => () => null
+      }
+    })
+    const wrapper = createWrapper({ propsData, store })
+    expect(wrapper.find(ProfileForm).exists()).toBeFalsy()
+    expect(wrapper.find(EditLinks).exists()).toBeFalsy()
+    expect(wrapper.find(EditSkills).exists()).toBeFalsy()
+    expect(wrapper.find(EditProjects).exists()).toBeFalsy()
+  })
+
   it('Should show correct components when profileId is not null', () => {
     const propsData = {
       profileId: '5'
     }
     const wrapper = createWrapper({ propsData })
     const profileFormWrapper = wrapper.find(ProfileForm)
+    const editSkillsWrapper = wrapper.find(EditSkills)
+    const editLinksWrapper = wrapper.find(EditLinks)
+    const EditProjectsWrapper = wrapper.find(EditProjects)
     expect(profileFormWrapper.isVisible()).toBeTruthy()
+    expect(profileFormWrapper.props().profile).toBe(mockProfile)
+    expect(editSkillsWrapper.isVisible()).toBeTruthy()
+    expect(editSkillsWrapper.props().profileId).toBe(mockProfile.id)
+    expect(editLinksWrapper.isVisible()).toBeTruthy()
+    expect(editLinksWrapper.props().profile).toBe(mockProfile)
+    expect(EditProjectsWrapper.isVisible()).toBeTruthy()
+    expect(EditProjectsWrapper.props().profileId).toBe(mockProfile.id)
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
