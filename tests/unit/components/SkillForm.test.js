9c9bd0d0c928777d44d2e46d0bc9df19d5e28aa5
Saara Pakarinen <saarapakarinen@Saaras-MacBook-Pro.local>
2018-11-06 12:35:24 +0200

    testit pois

--- tests/unit/components/SkillForm.test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import { SkillForm } from '@/components/Skills'
-
-describe('SkillForm.test.js', () => {
-  it('Template is correct', () => {
-    let wrapper = mount(SkillForm, {})
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
