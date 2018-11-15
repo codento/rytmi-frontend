0f84cf90468cac308d7f1fac77f5410b84da3d3d
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2018-11-15 16:44:51 +0200

    Two new tests (profile sorting). Refactored code to meet lint's needs.

--- tests/unit/components/Results.test.js
+++ tests/unit/components/Results.test.js
@@ -1,12 +1,9 @@
 import Vuex from 'vuex'
 import BootstrapVue from 'bootstrap-vue'
-import { merge } from 'lodash'
+import { merge, filter, isEmpty } from 'lodash'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import { Results, ProfileCard } from '@/components/Search'
-import flushPromises from 'flush-promises'
 import Loading from '@/components/lib/loading'
-import _ from 'lodash'
-
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -14,15 +11,15 @@ localVue.use(BootstrapVue)
 localVue.component('loading', Loading)
 
 const profilesMock = {
-  1: { id: 1, firstName: 'Joe'}, // Python(1,2), JavaScript(3,4)
-  2: { id: 2, firstName: 'Sarah'}, // Python(2,3), Scala(1,4)
-  3: { id: 3, firstName: 'Daisy'} // Python(5,4), JavaScript(3,3), Scala(2,2)
+  1: { id: 1, firstName: 'Joe' }, // Python(1,2), JavaScript(3,4)
+  2: { id: 2, firstName: 'Sarah' }, // Python(2,3), Scala(1,4)
+  3: { id: 3, firstName: 'Daisy' } // Python(5,4), JavaScript(3,3), Scala(2,2)
 }
 
 const skillsMock = {
-    1: { id: 1, name: 'Python' },
-    2: { id: 2, name: 'JavaScript' },
-    3: { id: 3, name: 'Scala' }
+  1: { id: 1, name: 'Python' },
+  2: { id: 2, name: 'JavaScript' },
+  3: { id: 3, name: 'Scala' }
 }
 
 const profileSkills = {
@@ -35,12 +32,14 @@ const profileSkills = {
   7: { id: 7, profileId: 3, skillId: 3, knows: 2, wantsTo: 2 }
 }
 
+const sortAttributeEnum = Object.freeze({ name: 1, wantsTo: 2, knows: 3 })
+
 function createStore (overrideConfig) {
   const defaultStoreConfig = {
     getters: {
       skills: () => skillsMock,
-      profileFilter: () => (name) => !_.isEmpty(name) ? _.filter(profilesMock, profile => profile.firstName === name) : Object.values(profilesMock),
-      skillsByProfileId: () => (profileId) => _.filter(profileSkills, ps => ps.profileId === profileId),
+      profileFilter: () => (name) => !isEmpty(name) ? filter(profilesMock, profile => profile.firstName === name) : Object.values(profilesMock),
+      skillsByProfileId: () => (profileId) => filter(profileSkills, ps => ps.profileId === profileId),
       profiles: () => profilesMock
     }
   }
@@ -93,7 +92,7 @@ describe('Results.vue', () => {
     })
   })
 
-  it('should show only filteres profiles when filtered by skills', (done) => {
+  it('should show only filtered profiles when filtered by skills', (done) => {
     const mountOptions = {
       propsData: {
         filterSkills: [{ id: 1, name: 'Python' }, { id: 2, name: 'JavaScript' }],
@@ -103,6 +102,46 @@ describe('Results.vue', () => {
     const expectedOrder = ['Daisy', 'Joe']
     const wrapper = createWrapper(mountOptions)
     wrapper.vm.$nextTick(() => {
+      const cards = wrapper.findAll(ProfileCard)
+      expect(cards).toHaveLength(2)
+      cards.wrappers.forEach((wrapper, i) => {
+        expect(wrapper.props().profile.firstName).toBe(expectedOrder[i])
+      })
+      done()
+    })
+  })
+
+  it('should show profiles ordered by willingness level for a skill', (done) => {
+    const mountOptions = {
+      propsData: {
+        filterSkills: [{ id: 3, name: 'Scala' }],
+        filterName: ''
+      }
+    }
+    const expectedOrder = ['Sarah', 'Daisy']
+    const wrapper = createWrapper(mountOptions)
+    wrapper.vm.$nextTick(() => {
+      wrapper.setData({ sortAttribute: sortAttributeEnum.wantsTo })
+      const cards = wrapper.findAll(ProfileCard)
+      expect(cards).toHaveLength(2)
+      cards.wrappers.forEach((wrapper, i) => {
+        expect(wrapper.props().profile.firstName).toBe(expectedOrder[i])
+      })
+      done()
+    })
+  })
+
+  it('should show profiles ordered by knowledge level for a skill', (done) => {
+    const mountOptions = {
+      propsData: {
+        filterSkills: [{ id: 3, name: 'Scala' }],
+        filterName: ''
+      }
+    }
+    const expectedOrder = ['Daisy', 'Sarah']
+    const wrapper = createWrapper(mountOptions)
+    wrapper.vm.$nextTick(() => {
+      wrapper.setData({ sortAttribute: sortAttributeEnum.knows })
       const cards = wrapper.findAll(ProfileCard)
       expect(cards).toHaveLength(2)
       cards.wrappers.forEach((wrapper, i) => {
