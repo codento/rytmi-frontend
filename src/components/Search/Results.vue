d342f1e0692d99c90dfc4dbad0056b40a79cdeb9
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2018-11-16 10:00:15 +0200

    Results component's code linted

--- src/components/Search/Results.vue
+++ src/components/Search/Results.vue
@@ -27,10 +27,10 @@
 </template>
 
 <script>
-import { isEmpty, intersection, debounce, keyBy, max as _max, sortBy } from 'lodash'
+import { isEmpty, sortBy } from 'lodash'
 import { mapGetters } from 'vuex'
 import ProfileCard from './ProfileCard'
-const sortAttributeEnum = Object.freeze({name:1, wantsTo:2, knows:3})
+const sortAttributeEnum = Object.freeze({ name: 1, wantsTo: 2, knows: 3 })
 
 export default {
   name: 'Results',
@@ -43,7 +43,7 @@ export default {
   },
   data () {
     return {
-      filteredProfiles: {},   
+      filteredProfiles: {},
       sortAttribute: sortAttributeEnum.name,
       profilesWithSkills: null
     }
@@ -69,58 +69,57 @@ export default {
     filteredSkillIds () {
       return this.filterSkills.map(skill => skill.id)
     },
-    orderedProfiles () {      
-      
-      //Every profile with skills, already filtered by name
+    orderedProfiles () {
+      // Every profile with skills, already filtered by name
       let filteredProfilesWithSkills = this.mapSkillsToProfiles()
-      
-      //Filter by skill(s)
+
+      // Filter by skill(s)
       this.filteredSkillIds.forEach(skillId => {
         filteredProfilesWithSkills = this.getProfilesBySkill(filteredProfilesWithSkills, skillId)
       })
 
-      //Sort profiles
+      // Sort profiles
       filteredProfilesWithSkills = this.sortProfiles(filteredProfilesWithSkills)
 
-      //Mapped skills aren't needed anymore from this point on, return only profiles      
+      // Mapped skills aren't needed anymore from this point on, return only profiles
       return filteredProfilesWithSkills.map(profileWithSkills => profileWithSkills.profile)
-    },  
-  },  
+    }
+  },
   methods: {
     getProfilesBySkill (profilesToFilter, skillId) {
       return profilesToFilter.filter(profile => profile.skills.filter(skill => skill.skillId === skillId).length > 0)
     },
-    mapSkillsToProfiles () {      
-      //First, get profiles, filtered by name
+    mapSkillsToProfiles () {
+      // First, get profiles, filtered by name
       const profilesWithoutSkills = Object.values(this.profileFilter(this.filterName))
-      //Then map skills for each profile
+      // Then map skills for each profile
       const profilesWithSkills = []
-      profilesWithoutSkills.forEach(profile => {             
+      profilesWithoutSkills.forEach(profile => {
         profilesWithSkills.push({
           profile: profile,
           skills: this.skillsByProfileId(profile.id)
-        })        
-      })      
+        })
+      })
       return profilesWithSkills
     },
     sortProfiles (profilesWithSkills) {
-      if (this.sortAttribute === sortAttributeEnum.name) {        
-        return sortBy(profilesWithSkills, ['profile.firstName'])  
+      if (this.sortAttribute === sortAttributeEnum.name) {
+        return sortBy(profilesWithSkills, ['profile.firstName'])
       }
-      if (this.sortAttribute === sortAttributeEnum.wantsTo || this.sortAttribute === sortAttributeEnum.knows) { //Redundant if, but clarifies code.
-          if (this.filteredSkillIds.length < 1) {
-            return profilesWithSkills
-          }
-          const skillToSortBy = this.filteredSkillIds[0] //TODO: What should be done here if several skills are selected?
-          const propertyToSortBy = (this.sortAttribute === sortAttributeEnum.wantsTo ? "wantsTo" : "knows")
-          return profilesWithSkills.sort(function (profile1, profile2) {
-            const profile1SkillLevel = getProfileSkill(profile1, skillToSortBy, propertyToSortBy)
-            const profile2SkillLevel = getProfileSkill(profile2, skillToSortBy, propertyToSortBy)        
-            return profile2SkillLevel - profile1SkillLevel
-          })         
+      if (this.sortAttribute === sortAttributeEnum.wantsTo || this.sortAttribute === sortAttributeEnum.knows) { // Redundant if, but clarifies code.
+        if (this.filteredSkillIds.length < 1) {
+          return profilesWithSkills
+        }
+        const skillToSortBy = this.filteredSkillIds[0] // TODO: What should be done here if several skills are selected?
+        const propertyToSortBy = (this.sortAttribute === sortAttributeEnum.wantsTo ? 'wantsTo' : 'knows')
+        return profilesWithSkills.sort(function (profile1, profile2) {
+          const profile1SkillLevel = getProfileSkill(profile1, skillToSortBy, propertyToSortBy)
+          const profile2SkillLevel = getProfileSkill(profile2, skillToSortBy, propertyToSortBy)
+          return profile2SkillLevel - profile1SkillLevel
+        })
       }
 
-      function getProfileSkill(profile, skillId, propertyToReturn) {
+      function getProfileSkill (profile, skillId, propertyToReturn) {
         return profile.skills.filter(skill => skill.skillId === skillId)[0][propertyToReturn]
       }
     }
