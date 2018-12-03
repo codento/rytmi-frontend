1066842afa0be11dc691c1b8e53f619796e9a215
vlilja <valtteri.lilja@gmail.com>
2018-12-03 09:56:03 +0200

    Utilization chart (#110)
    * Created utilization chart, added margin to dashboard
    
    * renamed chart and fixex styling
    
    * Tests for ConstultantUtilizationChart, restructured code
    
    * lint fixes

---
+++ src/components/Dashboard/ConsultantUtilizationChart.vue
@@ -0,0 +1,140 @@
+<template>
+  <chart-card>
+    <template slot="header">
+      <div class="h3 px-2">
+        Utilization
+      </div>
+    </template>
+    <b-col
+      :cols="12"
+      :md="12"
+    >
+      <p class="lead px-2">
+        Consultants in project today: <strong>{{ utilized }}</strong><br>
+        Consultants without project: <strong>{{ notUtilized }} </strong>
+      </p>
+    </b-col>
+    <b-col
+      :cols="12"
+      :md="12"
+    >
+      <line-chart
+        :chart-data="lineChartData"
+        :options="lineChartOptions"
+      />
+    </b-col>
+  </chart-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { isWithinRange, isBefore, addMonths, addWeeks, format } from 'date-fns'
+import ChartCard from './ChartCard'
+import LineChart from '../Charts/LineChart'
+
+const createLabel = (date) => format(date, 'DD/MM/YYYY')
+
+export default {
+  components: {
+    ChartCard,
+    LineChart
+  },
+  data () {
+    return {
+      today: Date.now(),
+      endDate: addMonths(Date.now(), 6),
+      utilized: 0,
+      notUtilized: 0,
+      activeProfiles: [],
+      chartTitleFontSize: '16',
+      chartColor: 'rgb(66, 244, 92, 0.4)',
+      chartBorderColor: 'rgb(66, 244, 92)'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'profileFilter',
+      'futureProjectsOfProfile'
+    ]),
+    lineChartData () {
+      const utilization = this.mapUtilizationOnTimeFrame(this.today, this.endDate)
+      return {
+        labels: utilization.labels,
+        datasets: [
+          {
+            data: utilization.data,
+            backgroundColor: this.chartColor,
+            borderColor: this.chartBorderColor
+          }
+        ]
+      }
+    },
+    lineChartOptions () {
+      return {
+        title: {
+          display: true,
+          fontSize: this.chartTitleFontSize,
+          text: 'Number of consultants in project'
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                suggestedMin: 0,
+                suggestedMax: Object.keys(this.profileFilter()).length
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  created () {
+    const profiles = this.profileFilter()
+    this.activeProfiles = Object.keys(profiles).map((key) => profiles[key].id)
+    this.activeProfiles.forEach((profile) => {
+      const projectProfiles = this.futureProjectsOfProfile(profile)
+      this.consultantHasOngoingProject(projectProfiles, this.today)
+        ? this.utilized += 1
+        : this.notUtilized += 1
+    })
+  },
+  methods: {
+    consultantHasOngoingProject (projectProfiles, date) {
+      if (projectProfiles.length > 0) {
+        const onGoingProjects = projectProfiles.filter(
+          pp => (isWithinRange(date, pp.startDate, pp.endDate))
+        )
+        return onGoingProjects.length > 0
+      }
+      return false
+    },
+    countUtilizedConsultantsOnDate (currentDate) {
+      let count = 0
+      this.activeProfiles.forEach(profile => {
+        const profileProjects = this.futureProjectsOfProfile(profile)
+        if (this.consultantHasOngoingProject(profileProjects, currentDate)) {
+          count += 1
+        }
+      })
+      return count
+    },
+    mapUtilizationOnTimeFrame (startDate, endDate) {
+      const utilization = []
+      const labels = []
+      let currentDate = startDate
+      for (let i = 0; isBefore(currentDate, endDate); i += 1) {
+        utilization.push(this.countUtilizedConsultantsOnDate(currentDate))
+        labels.push(createLabel(currentDate))
+        currentDate = addWeeks(currentDate, 1)
+      }
+      return { data: utilization, labels }
+    }
+  }
+}
+</script>
---
+++ tests/unit/components/ConsultantUtilizationChart.test.js
@@ -0,0 +1,116 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
+import LineChart from '@/components/Charts/LineChart'
+import { merge } from 'lodash'
+import ConsultantUtilizationChart from '@/components/Dashboard/ConsultantUtilizationChart'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(BootstrapVue)
+const dateNow = jest.spyOn(Date, 'now')
+const firstOfNovember = new Date('2018-11-01')
+dateNow.mockImplementation(() => firstOfNovember)
+
+const mockProfiles = {
+  1: { id: '1' },
+  2: { id: '2' },
+  3: { id: '3' },
+  4: { id: '4' },
+  5: { id: '5' }
+}
+const mockProjectProfiles = [
+  {
+    projectId: 1,
+    profileId: '1',
+    startDate: new Date('2018-11-01'),
+    endDate: new Date('2018-12-31')
+  },
+  {
+    projectId: 1,
+    profileId: '2',
+    startDate: new Date('2018-11-01'),
+    endDate: new Date('2018-11-30')
+  },
+  {
+    projectId: 2,
+    profileId: '2',
+    startDate: new Date('2018-12-15'),
+    endDate: new Date('2018-12-31')
+  },
+  {
+    projectId: 2,
+    profileId: '3',
+    startDate: new Date('2018-12-01'),
+    endDate: new Date('2018-12-31')
+  },
+  {
+    projectId: 2,
+    profileId: '4',
+    startDate: new Date('2018-12-01'),
+    endDate: new Date('2018-12-31')
+  },
+  {
+    projectId: 3,
+    profileId: '5',
+    startDate: new Date('2018-11-28'),
+    endDate: new Date('2018-12-31')
+  }
+]
+function createStore (overrideConfig) {
+  const defaultStoreConfig = {
+    getters: {
+      profileFilter: () => () => mockProfiles,
+      futureProjectsOfProfile: () => (id) => mockProjectProfiles.filter(pp => {
+        return pp.profileId === id
+      })
+    }
+  }
+  const mergedConfig = merge(defaultStoreConfig, overrideConfig)
+  return new Vuex.Store(mergedConfig)
+}
+
+function createWrapper (overrideMountingOptions) {
+  const defaultMountingOptions = {
+    localVue,
+    store: createStore()
+  }
+  const mergedMountingOptions = merge(defaultMountingOptions, overrideMountingOptions)
+  return shallowMount(ConsultantUtilizationChart, mergedMountingOptions)
+}
+
+describe('ConsultantUtilizationChart.vue', () => {
+  it('Should show line chart', () => {
+    const wrapper = createWrapper()
+    expect(wrapper.find(LineChart).isVisible()).toBeTruthy()
+  })
+
+  it('Should show correct amount of consultants utilized', () => {
+    const wrapper = createWrapper()
+    const utilized = 2
+    const notUtilized = 3
+    expect(wrapper.vm.utilized).toEqual(utilized)
+    expect(wrapper.vm.notUtilized).toEqual(notUtilized)
+  })
+
+  it('Should calculate consultant utilization on frequency of one week', () => {
+    const wrapper = createWrapper()
+    const expectedLabels = [
+      '01/11/2018',
+      '08/11/2018',
+      '15/11/2018',
+      '22/11/2018',
+      '29/11/2018',
+      '06/12/2018',
+      '13/12/2018',
+      '20/12/2018',
+      '27/12/2018',
+      '03/01/2019'
+    ]
+    const expectedUtilization = [2, 2, 2, 2, 3, 4, 4, 5, 5, 0]
+    const result = wrapper.vm.mapUtilizationOnTimeFrame(firstOfNovember, new Date('2019-01-07'))
+    expect(result.data.length).toBe(result.labels.length)
+    expect(result.data).toEqual(expectedUtilization)
+    expect(result.labels).toEqual(expectedLabels)
+  })
+})
