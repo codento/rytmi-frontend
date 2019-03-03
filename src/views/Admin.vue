1d8e2865daa128542b430747a956ea0cc649c2da
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-03-03 19:41:39 +0200

    Admid view template

---
+++ src/views/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container>
+    <b-row>ADMIN VIEW</b-row>
+  </b-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Admin',
+  computed: {
+    ...mapGetters(['profiles'])
+  }
+}
+</script>
