57da0883355f18c8da5ea120a6034fd0bcbba0d8
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-03-04 13:50:15 +0200

    Added EditUser card

---
+++ src/components/Admin/EditUser.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <b-card :title="fullName">
+      <div>
+        <b-form-group>
+          <b-form-checkbox v-model="isAdmin"> Is admin? </b-form-checkbox>
+        </b-form-group>
+        <b-form-group>
+          <b-row>
+            <b-col sm="3">
+              <b-button variant="success">Update</b-button>
+            </b-col>
+            <b-col sm="3" offset-sm="6">
+               <b-button variant="danger">Delete</b-button>
+            </b-col>
+          </b-row>
+        </b-form-group>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    },
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isAdmin: true
+    }
+  },
+  computed: {
+    fullName () {
+      const { firstName, lastName } = this.profile
+      return `${firstName} ${lastName}`
+    }
+  }
+}
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,9 +3,13 @@
     <h2>Admin</h2>
     <b-row>
       <b-col sm="12" md="6">
-        <UserList :users="profiles"/>
+        <UserList :users="profiles" @user-selected="setUser"/>
       </b-col>
-      <b-col sm="12" md="6">selected profile</b-col>
+      <b-col sm="12" md="6">
+        <div id="edit-user-card">
+          <EditUser v-if="selectedUser && selectedProfile" :user="selectedUser" :profile="selectedProfile" />
+        </div>
+      </b-col>
     </b-row>
   </b-container>
 </template>
@@ -13,13 +17,36 @@
 <script>
 import { mapGetters } from 'vuex'
 import UserList from '@/components/Admin/UserList'
+import EditUser from '@/components/Admin/EditUser'
 
 export default {
   components: {
-    UserList
+    UserList,
+    EditUser
+  },
+  data () {
+    return {
+      selectedUser: null,
+      selectedProfile: null
+    }
   },
   computed: {
     ...mapGetters(['profiles'])
+  },
+  methods: {
+    setUser (id) {
+      this.selectedUser = this.profiles[id]
+      this.selectedProfile = this.profiles[id]
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  @media only screen and (min-width: 992px) {
+     #edit-user-card {
+      position: fixed;
+      width: 550px;
+    }
+  }
+</style>
