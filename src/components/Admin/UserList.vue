9f36cee71dd45f6a23ce499d1d91e00ac50565d4
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-03-04 09:54:15 +0200

    Created user list to admin view

---
+++ src/components/Admin/UserList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-list-group
+      v-for="(user, idx) in usersAsList"
+      :key="idx"
+    >
+      <b-list-group-item
+        :active="isUserSelected(user.id)"
+        @click="selectUser(user.id)"
+      >
+        {{ user.firstName + ' ' + user.lastName }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    users: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selectedUserId: 50
+    }
+  },
+  computed: {
+    usersAsList () {
+      return Object.keys(this.users).map(id => this.users[id])
+    }
+  },
+  methods: {
+    selectUser (id) {
+      this.selectedUserId = id
+    },
+    isUserSelected (id) {
+      return this.selectedUserId === id
+    }
+  }
+}
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,23 @@
 <template>
   <b-container>
-    <b-row>ADMIN VIEW</b-row>
+    <h2>Admin</h2>
+    <b-row>
+      <b-col sm="12" md="6">
+        <UserList :users="profiles"/>
+      </b-col>
+      <b-col sm="12" md="6">selected profile</b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import UserList from '@/components/Admin/UserList'
+
 export default {
-  name: 'Admin',
+  components: {
+    UserList
+  },
   computed: {
     ...mapGetters(['profiles'])
   }
