b1eecaa98b0df61b74f78d08bb6ee587dbd03f1f
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-03-06 13:42:28 +0200

    Client api calls for user

--- src/utils/users/index.js
+++ src/utils/users/index.js
@@ -1,9 +1,16 @@
-import { get, put, post, del } from '../apiUtils'
+import { get, put, del } from '../apiUtils'
 
 const API_URL = process.env.VUE_APP_API_URL
 const PATH = API_URL + '/users'
 
 export const getUsers = async () => {
-  const users = await get(PATH)
-  return users
+  return get(PATH)
+}
+
+export const updateUser = (id, payload) => {
+  return put(PATH + '/' + id, payload)
+}
+
+export const deleteUser = (id) => {
+  return del(PATH + '/' + id)
 }
