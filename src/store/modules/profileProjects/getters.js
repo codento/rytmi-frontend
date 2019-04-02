b208b1879be78e12a1edfe7415b1fa3b69d70194
Riikka Nousiainen <riikka.nousiainen@codento.com>
2019-04-02 18:21:16 +0300

    Refactored variable names

--- src/store/modules/profileProjects/getters.js
+++ src/store/modules/profileProjects/getters.js
@@ -1,11 +1,11 @@
 export default {
   profileProjectsStatus: (state) => state.profileProjectsStatus,
   profileProjectsByProjectId: (state, getters, rootState) => (projectId) => {
-    const ppIds = rootState.projects.profileProjectList[projectId] || []
-    return ppIds.map(ppId => state.profileProjects[ppId])
+    const profileProjectIds = rootState.projects.profileProjectList[projectId] || []
+    return profileProjectIds.map(profileProjectId => state.profileProjects[profileProjectId])
   },
   profileProjectsByProfileId: (state, getters, rootState) => (profileId) => {
-    const ppIds = rootState.profiles.profileProjectList[profileId] || []
-    return ppIds.map(ppId => state.profileProjects[ppId])
+    const profileProjectIds = rootState.profiles.profileProjectList[profileId] || []
+    return profileProjectIds.map(profileProjectId => state.profileProjects[profileProjectId])
   }
 }
