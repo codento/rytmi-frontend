6a1d9032e58e0192595c7bbdef87e1d3e50b2825
Jan Nyholm <jan.nyholm@codento.com>
2019-04-12 12:42:54 +0300

    code cleanup

--- src/components/Search/ProfileCardProjectInfoRow.vue
+++ src/components/Search/ProfileCardProjectInfoRow.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <div
-      v-for="project in currentProjects(profile.id)"
+      v-for="project in futureProjectsOfProfile(profile.id)"
       :key="project.id"
     >
       <b-row class="project-info-row">
-        <b-col cols="8">
+        <b-col lg="12" xl="8">
           <b>{{ `${projectById(project.projectId).name}` }}</b>
         </b-col>
-        <b-col cols="2">
-          <b>End date:</b>
-        </b-col>
-        <b-col cols="2">
+        <b-col lg="12" xl="4">
+          <b>End date: </b>
           <b>{{ `${project.endDate ? formattedDate(project.endDate) : 'undefined'}` }}</b>
         </b-col>
       </b-row>
@@ -35,9 +33,6 @@ export default {
     ])
   },
   methods: {
-    currentProjects (id) {
-      return this.futureProjectsOfProfile(id).length > 0 ? this.futureProjectsOfProfile(id) : undefined
-    },
     formattedDate (date) {
       return format(date, 'D.M.YYYY')
     }
