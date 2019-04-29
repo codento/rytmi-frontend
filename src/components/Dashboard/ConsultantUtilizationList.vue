eed5f7ff9f95b1a0a36985fd019c154b9b06dfb2
Riikka Nousiainen <riikka.nousiainen@codento.com>
2019-04-29 22:19:23 +0300

    Add rotating arrow icon when toggling lists

--- src/components/Dashboard/ConsultantUtilizationList.vue
+++ src/components/Dashboard/ConsultantUtilizationList.vue
@@ -10,6 +10,8 @@
       class="mx-2 mb-1"
     >
       <b-card
+        v-for="(section, index) in sections"
+        :key="'utilization-list-' + index"
         no-body
         class="mb-1 rounded"
       >
@@ -19,22 +21,27 @@
           role="tab"
         >
           <b-button
-            id="toggle-consultants-without-projects"
-            v-b-toggle.consultants-without-projects
+            :id="'toggle-section-' + index"
+            v-b-toggle="'section-' + index"
             block
             variant="light"
+            @click.prevent="section.visible=!section.visible"
           >
-            Consultants without a project in the next 3 months
+            {{ section.heading }}
+            <i
+              :class="{ down: !section.visible }"
+              class="fa fa-chevron-up rotate"
+            />
           </b-button>
         </b-card-header>
         <b-collapse
-          id="consultants-without-projects"
-          visible
+          :id="'section-' + index"
+          :visible="section.initiallyVisible"
           role="tabpanel"
         >
           <b-card-body>
             <b-row
-              v-for="(item, index) in alwaysVisibleProfiles"
+              v-for="(item, itemIndex) in sectionsData[section.id]"
               :key="item.profile.id"
               class="align-items-end"
             >
@@ -54,71 +61,13 @@
                 class="mb-1"
               >
                 <utilization-chart
-                  v-if="index === 0"
+                  v-if="itemIndex === 0"
                   :projects="item.projects"
                   :override-options="chartOptionsForFirstItem"
                   height="25px"
                 />
                 <utilization-chart
-                  v-if="index > 0"
-                  :projects="item.projects"
-                  :override-options="overrideChartOptions"
-                  height="20px"
-                />
-              </b-col>
-            </b-row>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
-
-      <b-card
-        no-body
-        class="mb-1 rounded"
-      >
-        <b-card-header
-          header-tag="header"
-          class="p-1"
-          role="tab"
-        >
-          <b-button
-            id="toggle-consultants-in-projects"
-            v-b-toggle.consultants-in-projects
-            block
-            variant="light"
-          >
-            Consultants currently in a project
-          </b-button>
-        </b-card-header>
-        <b-collapse
-          id="consultants-in-projects"
-          role="tabpanel"
-        >
-          <b-card-body>
-            <b-row
-              v-for="(item, index) in initiallyHiddenProfiles"
-              :key="item.profile.id"
-              class="align-items-end"
-            >
-              <b-col
-                cols="2"
-                class="ml-4"
-              >
-                <span
-                  class="employee-name"
-                  @click="openProfile(item.profile)"
-                >
-                  {{ item.profile.firstName }} {{ item.profile.lastName }}
-                </span>
-              </b-col>
-              <b-col cols="9">
-                <utilization-chart
-                  v-if="index === 0"
-                  :projects="item.projects"
-                  :override-options="chartOptionsForFirstItem"
-                  height="25px"
-                />
-                <utilization-chart
-                  v-if="index > 0"
+                  v-if="itemIndex > 0"
                   :projects="item.projects"
                   :override-options="overrideChartOptions"
                   height="20px"
@@ -153,6 +102,20 @@ export default {
   props: {},
   data () {
     return {
+      sections: [
+        {
+          id: 1,
+          initiallyVisible: true,
+          visible: true,
+          heading: 'Consultants without a project in the next 3 months'
+        },
+        {
+          id: 2,
+          initiallyVisible: false,
+          visible: false,
+          heading: 'Consultants currently in a project'
+        }
+      ],
       hideThresholdInDays: 90,
       overrideChartOptions: {
         layout: {
@@ -208,15 +171,21 @@ export default {
       }
       return merge(optionsCopy, overrideOptions)
     },
-    alwaysVisibleProfiles () {
+    orderedProfiles () {
+      const mappedObjects = this.mapSkillsAndProjectsToProfiles()
+      return mappedObjects.sort((a, b) => a.utilization === 0 ? a.utilization - b.utilization : a.daysToZeroUtilization - b.daysToZeroUtilization)
+    },
+    initiallyVisibleProfiles () {
       return this.orderedProfiles.filter(item => item.daysToZeroUtilization <= this.hideThresholdInDays)
     },
     initiallyHiddenProfiles () {
       return this.orderedProfiles.filter(item => item.daysToZeroUtilization > this.hideThresholdInDays)
     },
-    orderedProfiles () {
-      const mappedObjects = this.mapSkillsAndProjectsToProfiles()
-      return mappedObjects.sort((a, b) => a.utilization === 0 ? a.utilization - b.utilization : a.daysToZeroUtilization - b.daysToZeroUtilization)
+    sectionsData () {
+      return {
+        1: this.initiallyVisibleProfiles,
+        2: this.initiallyHiddenProfiles
+      }
     }
   },
   methods: {
@@ -256,5 +225,15 @@ export default {
 }
 .employee-name:hover {
   font-weight: bolder;
+}
+.rotate {
+    -moz-transition: all .5s linear;
+    -webkit-transition: all .5s linear;
+    transition: all .5s linear;
+}
+.rotate.down {
+    -moz-transform:rotate(-180deg);
+    -webkit-transform:rotate(-180deg);
+    transform:rotate(-180deg);
 }
 </style>
