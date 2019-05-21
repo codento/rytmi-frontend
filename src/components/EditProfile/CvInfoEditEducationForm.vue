6507bd33642fafe4e2d86dbd2621338339449dc4
rnousia <riikka.nousiainen@codento.com>
2019-05-21 13:05:06 +0300

    Multi step form for adding education data

--- src/components/EditProfile/CvInfoEditEducationForm.vue
+++ src/components/EditProfile/CvInfoEditEducationForm.vue
@@ -1,56 +1,60 @@
 <template>
-  <b-tabs content-class="mt-1 border-0">
-    <b-tab
-      v-for="(langKey, tabIndex) in ['fi', 'en']"
-      :key="'tab-' + langKey"
-    >
-      <template slot="title">
-        {{ tabIndex == 0 ? 'In Finnish' : 'In English' }}
-        <span>
+  <div>
+    <h3>Edit education data (step {{ currentStep + 1 }} of {{ steps.length }})</h3>
+    <hr>
+    <b-row class="my-3">
+      <b-col
+        v-for="step in Array(steps.length).keys()"
+        :key="step"
+      >
+        <div
+          :class="step === currentStep ? 'step-title current-step' : 'step-title text-muted'"
+        >
           <i
-            v-if="!formIsValidByLanguage(langKey)"
-            class="fa fa-exclamation-circle text-danger"
+            v-if="step < currentStep"
+            class="fa fa-check"
           />
-          <i
-            v-else
-            class="fa fa-check-circle text-success"
-          />
-        </span>
-      </template>
+          <span v-else>
+            {{ step + 1 }}
+          </span>
+          <span> {{ steps[step].stepTitle }} </span>
+        </div>
+      </b-col>
+    </b-row>
+    <hr>
+    <div v-if="steps[currentStep].useFormTemplate">
       <b-row
-        v-for="(item, index) in labels.filter(item => item.key !== 'startYear' && item.key !== 'endYear')"
-        :key="kebabCase(item.key) + '-' + index"
+        v-for="item in formData.filter(item => item.key !== 'startYear' && item.key !== 'endYear')"
+        :key="kebabCase(item.key)"
       >
         <b-col cols="12">
+          <small :for="'input-'+ kebabCase(item.key) + '-' + currentLanguage">{{ item.label }}</small>
           <b-form-group
-            :id="'edit-'+ kebabCase(item.key) + '-' + langKey"
-            :label="item.label"
-            label-size="sm"
-            :label-for="'input-'+ kebabCase(item.key) + '-' + langKey"
-            :invalid-feedback="invalidFeedback(item.key, langKey)"
+            :id="'edit-'+ kebabCase(item.key) + '-' + currentLanguage"
+            :description="item.descriptions[currentLanguage]"
+            :invalid-feedback="invalidFeedback(item.key, currentLanguage)"
           >
             <b-form-input
-              :id="'input-'+ kebabCase(item.key) + '-' + langKey"
-              v-model="editedValues[langKey][item.key]"
+              :id="'input-'+ kebabCase(item.key) + '-' + currentLanguage"
+              v-model="editedValues[currentLanguage][item.key]"
               type="text"
               :required="item.key in requiredValues"
-              :state="inputState[item.key] ? inputState[item.key].state[langKey] : null"
+              :state="inputState[item.key] ? inputState[item.key].state[currentLanguage] : null"
               class="form-control"
             />
           </b-form-group>
         </b-col>
       </b-row>
-      <b-row>
+      <!-- Starting / finishing years are edited on first step only -->
+      <b-row v-if="currentStep === 0">
         <b-col
           v-for="key in ['startYear', 'endYear']"
           :key="key"
           cols="6"
         >
+          <small :for="'input-'+ kebabCase(key)">{{ formData.find(item => item.key === key).label }}</small>
           <b-form-group
             :id="'edit-' + kebabCase(key)"
-            :label="findLabel(key)"
-            label-size="sm"
-            :label-for="'input-'+ kebabCase(key)"
             :invalid-feedback="invalidFeedback(key)"
           >
             <b-input
@@ -59,64 +63,66 @@
               type="number"
               :min="1950"
               :max="new Date().getFullYear()"
+              :description="formData.find(item => item.key === key).descriptions[currentLanguage]"
               :state="editedValues[key] ? inputState[key].state : null"
             />
           </b-form-group>
         </b-col>
       </b-row>
-    </b-tab>
-    <b-card-group
-      v-for="(langKey, cardIndex) in ['fi', 'en']"
-      :key="'preview-in-' + langKey"
-      deck
-    >
-      <b-card
-        :header="cardIndex == 0 ? 'Preview in Finnish' : 'Preview in English'"
-        header-tag="header"
-        class="my-2 education-preview"
-        no-body
+    </div>
+    <div v-else>
+      <b-card-group
+        v-for="langKey in Object.keys(usedLanguages)"
+        :key="'preview-in-' + langKey"
+        deck
       >
-        <b-card-body>
-          <b-card-text>
-            <h4> {{ editedValues[langKey].school }} <span class="small"> {{ editedValues.startYear }} - {{ editedValues.endYear }}</span></h4>
-            <h5 v-if="editedValues[langKey].degree">
-              {{ editedValues[langKey].degree }}
-            </h5>
-            <p>
-              <span v-if="editedValues[langKey].major">
-                Pääaine: {{ editedValues[langKey].major }}
-              </span>
-              <br>
-              <span v-if="editedValues[langKey].minor">
-                Sivuaine: {{ editedValues[langKey].minor }}
-              </span>
-            </p>
-          </b-card-text>
-        </b-card-body>
-      </b-card>
-    </b-card-group>
+        <b-card
+          class="my-2 education-preview"
+          no-header
+          no-body
+        >
+          <b-card-body>
+            <b-card-text>
+              <h4> {{ editedValues[langKey].school }} <span class="small"> {{ editedValues.startYear }} - {{ editedValues.endYear }}</span></h4>
+              <h5 v-if="editedValues[langKey].degree">
+                {{ editedValues[langKey].degree }}
+              </h5>
+              <p>
+                <span v-if="editedValues[langKey].major">
+                  Pääaine: {{ editedValues[langKey].major }}
+                </span>
+                <br>
+                <span v-if="editedValues[langKey].minor">
+                  Sivuaine: {{ editedValues[langKey].minor }}
+                </span>
+              </p>
+            </b-card-text>
+          </b-card-body>
+        </b-card>
+      </b-card-group>
+    </div>
     <b-btn
       id="save"
       class="pull-right"
       variant="success"
-      :disabled="!formIsValidByLanguage('fi') || !formIsValidByLanguage('en')"
-      @click.prevent="$emit('submit', editedValues)"
+      :disabled="!canContinue"
+      @click="handleNextButton()"
     >
-      Save
+      {{ currentStep === steps.length - 1 ? 'Save' : 'Next' }}
     </b-btn>
     <b-btn
       id="cancel"
       variant="danger"
       class="pull-right"
-      @click="cancelEdits"
+      @click="handleBackButton()"
     >
-      Cancel
+      {{ currentStep === 0 ? 'Cancel' : 'Back' }}
     </b-btn>
-  </b-tabs>
+  </div>
 </template>
 
 <script>
-import { kebabCase } from 'lodash'
+import { kebabCase, cloneDeep } from 'lodash'
 
 export default {
   name: 'CvInfoEditEducationForm',
@@ -124,19 +130,61 @@ export default {
     initialValues: {
       type: Object,
       required: true
-    },
-    labels: {
-      type: Array,
-      required: true
     }
   },
   data () {
     return {
-      editedValues: Object.assign({}, this.initialValues),
-      requiredValues: ['school']
+      editedValues: cloneDeep(this.initialValues),
+      requiredValues: ['school'],
+      currentStep: 0,
+      usedLanguages: {
+        fi: { label: 'Suomeksi' },
+        en: { label: 'in English' }
+      },
+      formData: [
+        { key: 'school',
+          label: 'Place of education',
+          descriptions: {
+            fi: 'Oppilaitoksen nimi, esim. Aalto-yliopisto',
+            en: 'Name of the shool or institution, e.g. Aalto University'
+          }
+        },
+        { key: 'degree',
+          label: 'Degree',
+          descriptions: {
+            fi: 'Tutkinnon nimi, esim. Tietotekniikan diplomi-insinööri',
+            en: 'e.g. M.Sc. (tech) in Computer Science'
+          }
+        },
+        { key: 'major',
+          label: 'Major',
+          descriptions: {
+            fi: 'Pääaineen/suuntautumisen nimi, esim. Ohjelmistotekniikka',
+            en: 'Name of the major/study track, e.g Computer Science'
+          }
+        },
+        { key: 'minor',
+          label: 'Minor',
+          descriptions: {
+            fi: 'Sivuaineen nimi, esim. Geoinformatiikka',
+            en: 'Name of the minor studied, e.g. Geoinformatics'
+          }
+        },
+        { key: 'startYear', label: 'Starting year', descriptions: { fi: 'Aloitusvuosi', en: '' } },
+        { key: 'endYear', label: 'Finishing year', descriptions: { fi: 'Lopetusvupsi', en: '' } }
+
+      ],
+      steps: [
+        { languageKey: 'fi', useFormTemplate: true, stepTitle: 'Fill data in Finnish' },
+        { languageKey: 'en', useFormTemplate: true, stepTitle: 'Fill data in English' },
+        { useFormTemplate: false, stepTitle: 'Review and submit' }
+      ]
     }
   },
   computed: {
+    currentLanguage () {
+      return this.steps[this.currentStep].languageKey
+    },
     startYearState () {
       const rules = [
         {
@@ -162,6 +210,13 @@ export default {
         startYear: this.startYearState,
         endYear: this.isValidYear(this.editedValues.endYear)
       }
+    },
+    canContinue () {
+      if (this.currentStep < this.steps.length) {
+        return this.formIsValidByLanguage(this.steps[this.currentStep].languageKey)
+      } else {
+        return Object.keys(this.usedLanguages).every(key => this.formIsValidByLanguage(key))
+      }
     }
   },
   watch: {
@@ -172,9 +227,6 @@ export default {
   methods: {
     kebabCase (text) {
       return kebabCase(text)
-    },
-    findLabel (key) {
-      return this.labels.find(label => label.key === key).label
     },
     isValidYear (year) {
       return {
@@ -205,15 +257,36 @@ export default {
       })
       return statesForLanguage.every(state => state)
     },
-    cancelEdits () {
-      this.editedValues = Object.assign({}, this.initialValues)
-      this.$emit('cancel')
+    handleNextButton () {
+      if (this.currentStep < this.steps.length - 1) {
+        ++this.currentStep
+      } else {
+        this.$emit('submit', this.editedValues)
+      }
+    },
+    handleBackButton () {
+      if (this.currentStep > 0) {
+        --this.currentStep
+      } else {
+        this.editedValues = Object.assign({}, this.initialValues)
+        this.$emit('cancel')
+      }
     }
   }
 }
 </script>
 
 <style scoped >
+.step-title {
+  font-weight: 500;
+  font-size: 15px;
+  text-transform: uppercase;
+}
+
+.step-title.current-step {
+  font-weight: bolder;
+}
+
 .education-preview > h4 {
   font-weight: bolder;
 }
