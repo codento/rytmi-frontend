3fb6a66967cb852e5c78cf6e3415a8242df6b5b7
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2019-06-11 10:29:02 +0300

    Refactoring, restyling

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,12 +2,10 @@ import SkillList from './SkillList.vue'
 import SkillListEditSkill from './SkillListEditSkill.vue'
 import SkillListAdminPanel from './SkillListAdminPanel.vue'
 import SkillListAdminPanelItem from './SkillListAdminPanelItem.vue'
-import SkillLevelSelect from './SkillLevelSelect.vue'
 
 export {
   SkillList,
   SkillListEditSkill,
   SkillListAdminPanel,
-  SkillListAdminPanelItem,
-  SkillLevelSelect
+  SkillListAdminPanelItem
 }
