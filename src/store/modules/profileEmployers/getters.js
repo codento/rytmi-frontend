3779bd906381b1e87b23b5579fff811a1464cd61
Jan Nyholm <jan.nyholm@codento.com>
2019-06-20 15:46:11 +0300

    fix empty state.profileEmployers causing errors

--- src/store/modules/profileEmployers/getters.js
+++ src/store/modules/profileEmployers/getters.js
@@ -1,6 +1,6 @@
 export default {
   profileEmployers: state => state.profileEmployers,
   profileEmployersByProfileId: state => profileId => {
-    return Object.values(state.profileEmployers).filter(profileEmployer => profileEmployer.profileId === profileId)
+    return state.profileEmployers ? Object.values(state.profileEmployers).filter(profileEmployer => profileEmployer.profileId === profileId) : []
   }
 }
