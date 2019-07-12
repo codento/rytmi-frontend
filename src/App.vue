e749a6ffb77817b0551f8b66571c059b90f9e4c8
rnousia <riikka.nousiainen@codento.com>
2019-07-12 14:33:18 +0300

    Remove obsolete packages

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,6 @@
 import { mapActions, mapGetters, mapMutations } from 'vuex'
 import { SET_APP_INITIALIZED, SET_APP_INITIALIZE_ERROR } from '@/store/mutation-types'
 import {
-  Sidebar,
   Header as AppHeader,
   Footer as AppFooter
 } from './components/appStructures'
@@ -41,7 +40,6 @@ export default {
   name: 'App',
   components: {
     AppHeader,
-    Sidebar,
     AppFooter
   },
   data () {
@@ -183,16 +181,9 @@ export default {
 </script>
 
 <style lang="scss">
-// CoreUI Icons Set
-@import "~@coreui/icons/css/coreui-icons.min.css";
 /* Import Font Awesome Icons Set */
 $fa-font-path: "~font-awesome/fonts/";
 @import "~font-awesome/scss/font-awesome.scss";
-/* Import Simple Line Icons Set */
-$simple-line-font-path: "~simple-line-icons/fonts/";
-@import "~simple-line-icons/scss/simple-line-icons.scss";
-/* Import Flag Icons Set */
-@import "~flag-icon-css/css/flag-icon.min.css";
 /* Import Bootstrap Vue Styles */
 @import "~bootstrap-vue/dist/bootstrap-vue.css";
 // Import Main styles for this application
