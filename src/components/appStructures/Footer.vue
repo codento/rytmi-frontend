20fce050beb57e453dcc8eabfcaf52883017c2ab
Jan Nyholm <jan.nyholm@codento.com>
2019-07-16 11:02:48 +0300

    fix footer position and color

--- src/components/appStructures/Footer.vue
+++ src/components/appStructures/Footer.vue
@@ -1,38 +1,45 @@
 <template>
-  <b-container>
+  <b-container
+    fluid
+    class="footer"
+  >
     <b-row>
-      <span>Rytmiapp &copy; 2019 <a href="https://codento.com">Codento Oy</a></span>
-      <div
-        v-b-modal="'developer-info'"
-        class="ml-auto"
-      >
-        {{ `Frontend: ${frontendVersion}` }}
-        <br>
-        {{ `Api:  ${backendVersion}` }}
-      </div>
-      <b-modal
-        id="developer-info"
-        ok-only
-        ok-title="Close"
-        ok-variant="light"
-      >
-        <b-row>
-          <div>User token: {{ !!isAuthenticated }}</div>
-        </b-row>
-        <b-row>
-          <div>Token is valid: {{ isTokenValid }} until {{ new Date(tokenValidTime * 1000) }}</div>
-        </b-row>
-        <b-button @click="expireToken">
-          Set token to expire in 30 seconds
-        </b-button>
-        <template
-          slot="modal-header"
+      <b-col class="text-center my-auto">
+        <span>Rytmiapp &copy; 2019 <a href="https://codento.com">Codento Oy</a></span>
+      </b-col>
+      <b-col class="text-center">
+        <span
+          v-b-modal="'developer-info'"
         >
-          <h4 class="modal-header-text">
-            Developer info
-          </h4>
-        </template>
-      </b-modal>
+          {{ `Frontend: ${frontendVersion}` }}
+          <br>
+          {{ `Api:  ${backendVersion}` }}
+        </span>
+        <b-modal
+          id="developer-info"
+          ok-only
+          ok-title="Close"
+          ok-variant="light"
+        >
+          <b-row>
+            <div>User token: {{ !!isAuthenticated }}</div>
+          </b-row>
+          <b-row>
+            <div>Token is valid: {{ tokenValidTimeString() }}</div>
+          </b-row>
+          <b-button @click="expireToken">
+            Set token to expire in 30 seconds
+          </b-button>
+          <template
+            slot="modal-header"
+          >
+            <h4 class="modal-header-text">
+              Developer info
+            </h4>
+          </template>
+        </b-modal>
+        </b-col>
+
     </b-row>
   </b-container>
 </template>
@@ -41,6 +48,7 @@
 import { mapGetters } from 'vuex'
 import { version as frontendVersion } from '../../../package'
 import { getVersion } from '@/utils/api/api'
+import { format } from 'date-fns'
 
 export default {
   name: 'CFooter',
@@ -63,7 +71,17 @@ export default {
     },
     expireToken () {
       this.$store.commit('auth/SET_TOKEN_EXPIRATION', Date.now() / 1000 + 30)
+    },
+    tokenValidTimeString () {
+      return format(new Date(this.tokenValidTime * 1000), 'DD.MM.YYYY HH:mm:ss')
     }
   }
 }
 </script>
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+.footer {
+  color: #FFF;
+  padding: 15px;
+}
+</style>
